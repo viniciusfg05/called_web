87a5b9668b906c081e63c2092279685fc57b0826
Vinicius-Descktop <vinicius.fg05@gmail.com>
2026-02-11 15:49:55 -0300

    Ajuste 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,19 @@ import "./page.css"
 import Search from "@/components/search";
 import Summary from "@/components/summary";
 import Header from "@/components/head/page";
+import { Suspense } from "react";
 
 export default function Home() {
 
   return (
     <div className="containerHome">
       <div className="contenteHome">
-        <Header />
-        <Search />
-        <Summary />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Header />
+          <Search />
+          <Summary />
+        </Suspense>
+
       </div>
     </div>
   );
